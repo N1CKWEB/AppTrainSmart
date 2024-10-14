038c6986f3d6b741638633236e8d02ff31d56959
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-10-14 11:56:07 -0300

    Agregamos otra ventana con las opciones y un scrollbar

--- GUI/AppTrainSmart_.py
+++ GUI/AppTrainSmart_.py
@@ -1,10 +1,51 @@
 import customtkinter as ctk
-from customtkinter import CTkImage
+from customtkinter import CTkImage,CTkScrollableFrame
 import tkinter as tk
 from tkinter import ttk, messagebox, PhotoImage, Image
 from PIL import Image, ImageTk
 import os
 
+
+class ScrollBar_1(ctk.CTkScrollableFrame):
+    def __init__(self, master, title, values):
+        super().__init__(master, label_text=title)
+        self.grid_columnconfigure(0, weight=1)
+        self.values = values
+        self.checkboxes = []
+        self.pack(pady=23)
+
+        for i, value in enumerate(self.values):
+            checkbox = ctk.CTkCheckBox(self, text=value)
+            checkbox.grid(row=i, column=0, padx=5, pady=5, sticky="ew")
+            self.checkboxes.append(checkbox)
+
+    def obtener(self):
+        checked_checkboxes = []
+        for checkbox in self.checkboxes:
+            if checkbox.get() == 1:
+                checked_checkboxes.append(checkbox.cget("text"))
+        return checked_checkboxes
+    
+class ScrollBar_2(ctk.CTkScrollableFrame):
+    def __init__(self,master,title,values):
+        super().__init__(master,label_text=title)
+        self.grid_columnconfigure(0,weight=1)
+        self.values=values
+        self.checkboxes=[]
+        
+        for i,value in enumerate(self.values):
+            checkbox=ctk.CTkCheckBox(self,text=value)
+            checkbox.grid(row=i,column=0,padx=5,pady=5,sticky='ew')
+            self.checkboxes.append(checkbox)
+            
+    def obtener(self):
+        checked_checkboxes = []
+        for checkbox in self.checkboxes:
+            if checkbox.get() == 1:
+                checked_checkboxes.append(checkbox.cget("text"))
+        return checked_checkboxes         
+
+        
 class TrainSmart(ctk.CTk):
     
     def __init__(self):
@@ -77,7 +118,8 @@ class TrainSmart(ctk.CTk):
         ventana_registro_de_entrenamiento = ctk.CTkToplevel(self)
         ventana_registro_de_entrenamiento.title('Registrar entrenamiento')
         ventana_registro_de_entrenamiento.geometry('300x500')  # Incrementa el tamaño de la ventana para ajustarse a las imágenes grandes.
-
+        ventana_registro_de_entrenamiento.resizable(0, 0)
+#       
         ventana_registro_de_entrenamiento.columnconfigure(0, weight=1)
         # BOTÓN/IMAGEN 1 - Inicia loguarse
         imagen_original_1 = Image.open('1.png')
@@ -86,7 +128,7 @@ class TrainSmart(ctk.CTk):
         boton_registrar_entrenamiento = ctk.CTkLabel(ventana_registro_de_entrenamiento, image=imagen_tk_1, fg_color="transparent", bg_color="transparent", text='')
         boton_registrar_entrenamiento.image = imagen_tk_1
         boton_registrar_entrenamiento.grid(row=0, column=0, padx=20, pady=(5, 10), sticky="n")  # Aumenta el espaciado y centra
-        boton_registrar_entrenamiento.bind("<Button-1>", self.loguarse)
+        boton_registrar_entrenamiento.bind("<Button-1>", self.registrar_entrenamiento)
 
         # BOTÓN/IMAGEN 2 - Inicia loguarse_2
         imagen_original_2 = Image.open('2.png')
@@ -95,7 +137,7 @@ class TrainSmart(ctk.CTk):
         boton_registrar_entrenamiento_futuro = ctk.CTkLabel(ventana_registro_de_entrenamiento, image=imagen_tk_2, fg_color="transparent", bg_color="transparent", text='')
         boton_registrar_entrenamiento_futuro.image = imagen_tk_2
         boton_registrar_entrenamiento_futuro.grid(row=1, column=0, padx=20, pady=(5, 10), sticky="n")  # Aumenta el espaciado
-        boton_registrar_entrenamiento_futuro.bind("<Button-1>", self.loguarse_2)
+        boton_registrar_entrenamiento_futuro.bind("<Button-1>", self.registrar_entrenamiento_futuro)
 
         # BOTÓN/IMAGEN 3 - Inicia loguarse_3
         imagen_original_3 = Image.open('3.png')
@@ -104,16 +146,51 @@ class TrainSmart(ctk.CTk):
         boton_seguimiento_registrado_anteriormente = ctk.CTkLabel(ventana_registro_de_entrenamiento, image=imagen_tk_3, fg_color='transparent', bg_color='transparent', text='')
         boton_seguimiento_registrado_anteriormente.image = imagen_tk_3
         boton_seguimiento_registrado_anteriormente.grid(row=2, column=0, padx=20, pady=(5, 10), sticky="n")  # Ajustar espaciado
-        boton_seguimiento_registrado_anteriormente.bind("<Button-1>", self.mostrar_boton_seguimiento_registrado_anteriormente)
+        boton_seguimiento_registrado_anteriormente.bind("<Button-1>", self.seguimiento_registrado_anteriormente)
+    
+    
+    def registrar_entrenamiento(self, event=None):
+        
+   # Creamos ventana para el primer botón
+        ventana_del_primer_boton = ctk.CTkToplevel(self)
+        ventana_del_primer_boton.title('Registrar entrenamiento realizado')
+        ventana_del_primer_boton.geometry('300x500')  # Ajustamos tamaño
 
-    def mostrar_boton_seguimiento_registrado_anteriormente(self, event=None):
-        print('Funcion loguarse_3 ejecutada')
+        # Hacemos que las columnas y filas se ajusten
+        ventana_del_primer_boton.columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0,weight=1)
 
-    def loguarse_2(self, event=None):
-        # Código para manejar el evento del segundo botón
-        print("Función loguarse_2 ejecutada")
+        # Agregar ScrollBar dentro de la ventana del primer botón
+        values_1 = ["Cardio", "Fuerza", "Flexibilidad"]
+        self.scrollable_checkbox_frame = ScrollBar_1(ventana_del_primer_boton, title="Tipo de Ejercicio", values=values_1)
+        self.scrollable_checkbox_frame.grid(row=0, column=0, padx=3, pady=(5, 10), sticky="nsew")
+        
+        values_2=['Pecho','Espalda','Piernas','Hombros','Bíceps','Tríceps']
+        self.scrollable_checkbox_frame_2=ScrollBar_2(ventana_del_primer_boton,title='Grupo muscular trabajado',values=values_2)
+        self.scrollable_checkbox_frame_2.grid(row=1, column=0, padx=3, pady=(5, 10), sticky="nsew")
+          
+        # values_3=['25m','30m','40m','45m','1h','2h']
+        # self.scrollable_checkbox_frame_3=ScrollBar_3(ventana_del_primer_boton,title='Duración del entrenamiento',values=values_3)
+        
+        
+          
+      
+      
+       #  BOTON PARA ENVIAR COMPLETO EL FORMULARIO  
+        self.button = ctk.CTkButton(ventana_del_primer_boton, text="my button", command=self.boton_enviar_registro)
+        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
 
-            # Llamar a las funciones para mostrar los botones
+
+    def boton_enviar_registro(self):
+        pass
+  
+    def registrar_entrenamiento_futuro(self, event=None):
+             pass
+    
+    def seguimiento_registrado_anteriormente(self, event=None):
+             pass
+
+    
         
     def mostrar_link_registro(self):
         # Crear un link de texto para registrarse
